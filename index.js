06fd087c8a405e8e85c39ab473dc9f715cf33ecd
Christian Gruenberg <c.gruenberg@lvm.de>
2016-02-17 12:53:03 +0100

    erste REST-Services implementiert

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 var express = require('./node_modules/express');
 var app = express();
-
+var server = 'localhost:8080';
 app.get('/',function(req,res,next){
     res.send('Herzlich Willkommen')
     next();
 });
+
+app.get('/partner',function(req,res,next){
+    var partners = [{partnerId:4711,partnerURI:`http://${server}/partner/4711`,name:'Produkt',vorname:'Peter',ort:'Münster',status:'Kunde'},
+                    {partnerId:4712,partnerURI:`http://${server}/partner/4713`,name:'Produkt',vorname:'Hans',ort:'Osnabrück',status:'Kunde'},
+                    {partnerId:4713,partnerURI:`http://${server}/partner/4712`,name:'Produkt',vorname:'Günther',ort:'Dortmund',status:'Interessent'}];
+    res.send(partners);
+    next();
+});
+app.get('/partner/:id',function(req,res,next) {
+   var partner = {partnerId:4711,partnerURI:`http://${server}/partner/4711`,name:'Produkt',vorname:'Peter',ort:'Münster',status:'Kunde',plz:48153,strasse:'Hammerstr. 50',tel:"0251123456679"};
+   res.send(partner);         
+   next(); 
+});
+
+app.get('/partner/:id/haushalt',function(req,res,next){
+   var haushalt = [{beziehung:'Frau',name:'Produkt',vorname:'Petra'},
+                    {beziehung:'Kinde',name:'Produkt',vorname:'Klaus'}];
+    res.send(haushalt);
+   
+   next(); 
+});
+app.get('/partner/:id/kontakt',function(req,res,next){
+   var kontakt = [{kontaktart:'Telefon',zeit:'2016-01-15 12:30',titel:'Beratung'},
+                    {kontaktart:'Email',zeit:'2016-01-15 12:40',titel:'Unterlagen'}];
+    res.send(kontakt);
+   
+   next(); 
+});
+
+app.get('/angebote',function(req,res,next){
+   var angebote = [{sparte='Kraftfahrt',beitragZent=10000}];
+   res.send(angebote);
+   next(); 
+});
+app.get('/antraege',function(req,res,next){
+   var angebote = [{sparte='Kraftfahrt',beitragZent=12000}];
+   res.send(angebote);
+   next(); 
+});
+app.get('/vertaege',function(req,res,next){
+   var angebote = [{sparte='Kraftfahrt',beitragZent=13000}];
+   res.send(angebote);
+   next(); 
+});
+
+
+
 
 // Use Case 1
 // '/partner&q='Peter Produkt'
